fc6701756b86359ceb792b4708f4b733c057de85
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2025-04-01 07:11:20 +0200

    Initial Commit

---
+++ AnimationsSwiftUI/AnimationsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsSwiftUIApp.swift
+//  AnimationsSwiftUI
+//
+//  Created by Manyuchi, Carrington C on 2025/04/01.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
